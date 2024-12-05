97b45c1f301a6e86aea276b02dd2874f26d9f282
yosf AL-otaibi <yousefalotaini21@gmail.com>
2024-12-05 11:45:45 +0300

    done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 8000;
 
 const accounts = require("./accounts");
 app.get("/", (req, res) => {
-  res.status({ name: "yousef" });
+  res.json({ name: "yousef" });
 });
 app.listen(port, () => {
   console.log(`the application is runing ${port}`);
